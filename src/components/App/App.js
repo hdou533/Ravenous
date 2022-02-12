ed36dc1a1c89ea3e9f678ef15ea096ca842e4678
Hongnan <55491203+hdou533@users.noreply.github.com>
2022-02-12 16:45:57 +1300

    add search bar state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
-import SearchBar from '../SearchBar/SearchBar'
+import SearchBar from '../SearchBar/SearchBar';
+
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'Hell Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 3.5,
+  reviewCount: 90
+}
+
+const businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
+  searchYelp(term,location,sortBy) {
+    console.log('Searching Yelp with Pizza, Brooklyn, best_match')
+  }
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList />
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList businesses={businesses} />
       </div>
     )
   }
